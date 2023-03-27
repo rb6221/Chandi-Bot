090bf99852b159be480b3d389595b0c7fca85688
MaxFusco <maxfusc627@gmail.com>
2023-03-26 22:41:14 -0400

    commands main structure

---
+++ commands/main.go
@@ -0,0 +1,81 @@
+package commands
+
+import (
+	"log"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/sirupsen/logrus"
+)
+
+var (
+	SlashCommands     map[string]*structs.SlashCommand                                      = make(map[string]*structs.SlashCommand)
+	Handlers          map[string]interface{}                                                = make(map[string]interface{})
+	ScheduledEvents   map[string]*structs.ScheduledEvent                                    = make(map[string]*structs.ScheduledEvent)
+	ComponentHandlers map[string]func(s *discordgo.Session, i *discordgo.InteractionCreate) = make(map[string]func(s *discordgo.Session, i *discordgo.InteractionCreate))
+
+	quit chan interface{} = make(chan interface{})
+)
+
+func Init() {
+	slash.Init(&ComponentHandlers)
+	scheduled.Init(&ComponentHandlers)
+	handlers.Init(&ComponentHandlers)
+
+	populateSlashCommands()
+	populateHandlers()
+	populateScheduledEvents()
+
+	addSlashCommands()
+	addHandlers()
+
+}
+
+func addSlashCommands() {
+	for _, command := range SlashCommands {
+		_, err := bot.Session.ApplicationCommandCreate(config.AppID, config.GuildID, command.Command)
+		if err != nil {
+			logrus.WithError(err).Error("Failed to register slash command")
+		} else {
+			logrus.Infof("Registered slash command: %s", command.Command.Name)
+		}
+	}
+
+	bot.Session.AddHandler(func(
+		s *discordgo.Session,
+		i *discordgo.InteractionCreate,
+	) {
+
+		switch i.Type {
+		case discordgo.InteractionApplicationCommand:
+			data := i.ApplicationCommandData()
+
+			if command, ok := SlashCommands[data.Name]; ok {
+				command.Handler(s, i)
+			}
+		case discordgo.InteractionMessageComponent:
+			if command, ok := ComponentHandlers[i.MessageComponentData().CustomID]; ok {
+				command(s, i)
+			}
+		}
+	})
+}
+
+func addHandlers() {
+	for name, handler := range Handlers {
+		bot.Session.AddHandler(handler)
+		log.Printf("Registered handler: %s", name)
+	}
+}
+
+func StartScheduledTasks() {
+	for name, event := range ScheduledEvents {
+		go event.Run(bot.Session, quit)
+		log.Printf("Starting scheduled task: %v\n", name)
+	}
+}
+
+func StopScheduledTasks() {
+	if len(ScheduledEvents) > 0 {
+		quit <- "kill"
+	}
+}
